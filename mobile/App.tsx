5ffc007a6a3883d4e6c02c12e8a49348b034f28f
ArthurRod <arthur9713@hotmail.com>
2022-05-06 18:56:33 -0300

    Component Botão Widget flutuante criado

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { Widget } from "./src/components/Widget";
+import { theme } from "./src/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View
+      style={{
+        backgroundColor: theme.colors.background,
+        flex: 1,
+      }}
+    >
+      <Widget />
+
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
